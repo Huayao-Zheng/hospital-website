f478cbd4787ea58a560a8aaf4f70b3dbdbbf698d
lonelybuddy <henryzheng92@gmail.com>
2022-03-12 08:55:31 -0600

    finished header

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ openNavBtn.addEventListener('click', () => {
   openNavBtn.style.display = 'none';
 });
 
-// do the opposite to the close__nav-btn, when it's clicked
 closeNavBtn.addEventListener('click', () => {
   navList.style.display = 'none';
   closeNavBtn.style.display = 'none';
