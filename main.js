66675053a1c30cae6d6016b55bb1fc330e39b84d
lonelybuddy <henryzheng92@gmail.com>
2022-03-16 12:22:51 -0500

    finished Testimonals section, and added Swiper.js for slides

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ closeNavBtn.addEventListener('click', () => {
   closeNavBtn.style.display = 'none';
   openNavBtn.style.display = 'inline-block';
 });
+
+// Testimonals (Swiper.js)
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
